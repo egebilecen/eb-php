511ed96662626e6989de102df9b9c7488fff851c
Ege Bilecen <egebilecen@icloud.com>
2021-05-06 21:08:49 +0300

    added post and get functions

--- request_util.php
+++ request_util.php
@@ -27,4 +27,41 @@ function r($key, $trim=true)
     return $data;
 }
 
+function send_get_request($url)
+{
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPGET, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36");
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    $data = curl_exec($curl);
+    $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+
+    return array($status_code, $data);
+}
+
+function send_post_request($url, $post_data)
+{
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post_data));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36");
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    $data = curl_exec($curl);
+    $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+
+    return array($status_code, $data);
+}
+
 ?>
