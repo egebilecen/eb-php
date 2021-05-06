1b8dad375f99cf8207cb029f580d1d30486872c7
Ege Bilecen <egebilecen@icloud.com>
2021-05-06 21:29:41 +0300

    added dynamic echo func

--- html_util.php
+++ html_util.php
@@ -24,4 +24,21 @@ function js_redirect_back($delay_ms = null)
     die();
 }
 
+function js_alert($str)
+{
+    echo "<script>alert('".$str."');</script>";
+}
+
+// ob_start() must be called before this function. And ob_end() must be called if there will be no more calls to this function.
+function dynamic_echo($str)
+{
+    echo "<script class='__php_echo_rem_js'>try{document.querySelector('.__php_dyn_echo').remove();}catch(ex){}document.querySelector('.__php_echo_rem_js').remove();</script>";
+    echo "<span class='__php_dyn_echo'>".$str."</span>";
+    
+    ob_end_flush(); 
+    ob_flush(); 
+    flush(); 
+    ob_start(); 
+}
+
 ?>
