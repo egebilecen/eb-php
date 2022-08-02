ccc34889ed4245b70b5cf394c496c1e1efefbbab
Ege Bilecen <egebilecen@icloud.com>
2022-08-02 07:44:38 +0300

    Create util.php

---
+++ util.php
@@ -0,0 +1,49 @@
+<?php
+
+function format_mysql_date($date)
+{
+    $formatted_str = "";
+    $date_split    = explode(" ", $date);
+    $year_week_day = explode("-", $date_split[0]);
+    $hour_min_sec  = explode(":", $date_split[1]);
+
+    $formatted_str .= $year_week_day[2] . "/" . $year_week_day[1] . "/" . $year_week_day[0];
+    $formatted_str .= ", ";
+    $formatted_str .= $hour_min_sec[0] . ":" . $hour_min_sec[1];
+
+    return $formatted_str;
+}
+
+function format_mysql_date_without_hour($date)
+{
+    $formatted_str = "";
+    $date_split    = explode(" ", $date);
+    $year_week_day = explode("-", $date_split[0]);
+
+    $formatted_str .= $year_week_day[2] . "/" . $year_week_day[1] . "/" . $year_week_day[0];
+
+    return $formatted_str;
+}
+
+function redirect($page)
+{
+    header("Location: ".$page);
+}
+
+function print_json($data)
+{
+    echo json_encode($data);
+}
+
+function return_json($data)
+{
+    die(print_json($data));
+}
+
+function debug_var($var)
+{
+    var_dump($var);
+    die();
+}
+
+?>
