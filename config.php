6ca54217eda6edc25a99255c753df0f7ac67fb55
Ege Bilecen <29331682+egebilecen@users.noreply.github.com>
2023-01-29 01:00:36 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,18 @@
 <?php
 
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
 date_default_timezone_set("Europe/Istanbul");
 set_time_limit(0);
 session_start();
 
-$db_host = 'localhost';
-$db_name = 'db_name';
-$db_user = 'root';
-$db_pass = '';
+$db_host = "localhost";
+$db_name = "db_name";
+$db_user = "root";
+$db_pass = "";
 
 try
 {
-    $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name.';', $db_user, $db_pass);
+    $db = new PDO("mysql:host=".$db_host.";dbname=".$db_name.";", $db_user, $db_pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // fix int being returned as string
 	
